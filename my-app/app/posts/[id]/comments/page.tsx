1a7cbd06c504d562bf637ed6b3ff623ed9ea9c44
leobema <leonardoberti21@gmail.com>
2024-03-01 20:23:52 -0300

    fix

--- my-app/app/posts/[id]/comments/page.tsx
+++ my-app/app/posts/[id]/comments/page.tsx
@@ -1,5 +1,5 @@
 
-const fetchComments = (id: number) => {
+const fetchComments = (id: Number) => {
     return fetch(`https://jsonplaceholder.typicode.com/posts/${id}/comments`, { 
       next: {
         revalidate: 60
@@ -8,13 +8,13 @@ const fetchComments = (id: number) => {
     .then(res => res.json())
   }
 
-export default async function Post({ id }: {id:number}) {
+export default async function Post({ id }: {id:Number}) {
     const comments = await fetchComments(id)
 
     return (
         <ul className=" bg-slate-200 mt-2">
-            {comments.map((comment:{id: number, name: string, body: string}) => (
-            <li key={comment.id} className=" p-4">
+            {comments.map((comment:{id: Number, name: string, body: string}) => (
+            <li key={+comment.id} className=" p-4">
                 <h2 className=" font-bold">{comment.name}</h2>
                 <p>{comment.body}</p>
             </li>
